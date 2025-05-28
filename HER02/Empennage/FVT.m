ef8315b726a09087ed2c2ec5ce266e70d800cee7
CreateProfile <78679497+CreateProfile@users.noreply.github.com>
2025-05-28 15:54:59 +0100

    Add files via upload

---
+++ HER02/Empennage/FVT.m
@@ -0,0 +1,25 @@
+function [LVT,DVT] = FVT(u,v,w,rho,delR)
+% Lift & Drag Force of Vertical Tailplane
+% Lift positive rightwards
+% Drag positive backwards
+% positive u in forward flight
+% positive v while drifting right
+% positive w while climbing
+% del R positive when airfoil points right (yaw left)
+
+HTl = 0.9; % Horizontail Tail length (airfoil chord length)
+HTt = HTl*0.15; % Horizontal Tail thickness
+VTl = HTl; % Vertical Tail length (airfoil chord length) SAME AS HTl
+VTw = 1; % Vertical Tail width (top to bottom length)
+
+Area = 2*VTl*VTw;
+
+alpha = -atand(v/u)+delR;
+
+[CL,CD] = NACA0015(alpha);
+LHT = CL*0.5*rho*u^2*Area;
+DHT = CD*0.5*rho*u^2*Area;
+
+
+
+end
