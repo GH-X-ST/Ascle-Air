0dc5c89034be5d041a9c7b5e5c2b8ff6c68086d4
Hanchen Li <lihanchen12476@163.com>
2025-06-07 00:35:46 +0100

    Force & Moment Function v5.2
    - Hopefully, this is the final version of the force-and-moment function
    - After a week of debugging and rewriting, all 18 sub-functions are complete and can handle general flight conditions
    - The verification code is in 3 folders (rotor aerodynamic, rotor dynamic and fuselage & empennage), you could verified them by input and run the live scripts inside folders
    - Caution: the function will produce abnormal results for angles of attack above 20 degrees, because stall effects are not yet modelled (no boundary has been implemented, please take this into account in your calculations)
    - More specific boundaries will be added later

---
+++ HER05/01 - Mathematical Modeling/Fuselage and Empennage Test/FVT.m
@@ -0,0 +1,23 @@
+function [LVT,DVT] = FVT(u,v,w,rho,delR)
+% Lift & Drag Force of Vertical Tailplane
+% Lift positive rightwards
+% Drag positive backwards
+% positive u in forward flight
+% positive v while drifting right
+% negative w while climbing
+% del R positive when airfoil points right (yaw left)
+
+HTl = 0.9; % Horizontail Tail length (airfoil chord length)
+HTt = HTl*0.15; % Horizontal Tail thickness
+VTl = HTl; % Vertical Tail length (airfoil chord length) SAME AS HTl
+VTw = 1; % Vertical Tail width (top to bottom length)
+
+Area = VTl*VTw;
+
+alpha = atan2d( v , u ) + rad2deg(delR);   % safe in all quadrants
+
+[CL,CD] = NACA0015(alpha);
+LVT = CL*0.5*rho*(u^2+v^2+w^2)*Area;
+DVT = CD*0.5*rho*(u^2+v^2+w^2)*Area;
+
+end
