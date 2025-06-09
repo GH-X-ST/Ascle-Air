c6f3b41a9467f4851b57bf88937e37ea65ac6d40
Hanchen Li <lihanchen12476@163.com>
2025-06-09 01:27:05 +0100

    Force & Moment Function v5.3
    - Small updates

--- HER05/01 - Mathematical Modeling/FM.m
+++ HER05/01 - Mathematical Modeling/FM.m
@@ -192,7 +192,8 @@ else
 
     % 4.3 Forward flight aerodynamics & dynamics
 
-    [T_UR, T_LR, H_UR, H_LR, Y_UR, Y_LR, Q_UR, Q_LR, Mx_UR, My_UR, Mx_LR, My_LR, ~, ~, beta1c_UR, beta1s_UR, beta1c_LR, beta1s_LR] = Stability_forward(u, v, w, a, rho, mu, m, g, polar, theta_UR, theta_LR, theta_1c, theta_1s);
+    [T_UR, T_LR, H_UR, H_LR, Y_UR, Y_LR, Q_UR, Q_LR, Mx_UR, My_UR, Mx_LR, My_LR, ~, ~, beta1c_UR, beta1s_UR, beta1c_LR, beta1s_LR] = ...
+        Stability_forward(u, v, w, a, rho, mu, m, g, polar, theta_UR, theta_LR, theta_1c, theta_1s);
 
     Q_UR = -Q_UR; % to reaction torque
 
